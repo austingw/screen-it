fd3f236dd8436e1f80378d1374883420e4c887f7
austingw <senortinman@gmail.com>
2023-01-18 13:21:13 -0500

    Add additional string methods to remove instances of the word 'the' from the string

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,16 @@
 export default function ListItem(props) {
   return (
     <div className="list-item">
-      <div className="item-icon">{props.name.match(/[A-Z]/g).join("")}</div>
+      <div className="item-icon">
+        {
+          //Chained string methods turn the movie name into an acronym
+          props.name
+            .replace("The", "")
+            .replace("the", "")
+            .match(/[A-Z]/g)
+            .join("")
+        }
+      </div>
       <div className="item-text">
         <div className="name">{props.name}</div>
         <div className="category">{props.category}</div>
